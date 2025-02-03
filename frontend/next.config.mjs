8f7ee8939e8d4e687ba20ca7f683f1abf88dc6dc
nacho995 <ignaciodalesio1995@gmail.com>
2025-02-03 11:40:15 +0100

    blogFetching

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+    experimental: {
+      appDir: true, // Asegura que Next.js use la carpeta "app"
+    },
   };
   
   export default nextConfig;
