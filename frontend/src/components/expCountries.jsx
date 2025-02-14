6a8c4bc02970efacd32ec6202fe5a4ca6c930aa1
nacho995 <ignaciodalesio1995@gmail.com>
2025-02-14 13:51:34 +0100

    expRealty

--- frontend/src/components/expCountries.jsx
+++ frontend/src/components/expCountries.jsx
@@ -1,43 +1,93 @@
-"use client"
+"use client";
 
-import AnimatedOnScroll from "./AnimatedScroll"
-
+import React from "react";
+import AnimatedOnScroll from "./AnimatedScroll";
 
 export default function ExpCountries() {
     return (
         <AnimatedOnScroll>
-            <div className="grid grid-cols-4 gap-y-0 md:grid-cols-2 md:gap-x-0 md:gap-y-0 lg:grid-cols-3 lg:gap-x-12 lg:gap-y-0">
-                <div>
-                    <div className="flex items-center justify-center h-full w-full bg-amarillo/20 rounded-lg">
-                        <img src="/spain.jpg" alt="Spain" className="h-full w-full" />
-                        <h3 className="absolute text-2xl font-bold text-center mt-4">Spain</h3>
-                    </div>
+            <div className="grid grid-cols-2 md:grid-cols-4 w-full overflow-hidden">
+                {/* Tarjeta 1 */}
+                <div className="relative  transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className="w-full h-full rounded-lg object-cover" />
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
                 </div>
-                <div>
-                    <div className="flex items-center justify-center h-full w-full bg-amarillo/20 rounded-lg">
-                        <img src="/spain.jpg" alt="Spain" className="h-full w-full" />
-                        <h3 className="absolute text-2xl font-bold text-center mt-4">Spain</h3>
-                    </div>
+                {/* Tarjeta 2 */}
+                <div className="relative transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover" />
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
                 </div>
-                <div>
-                    <div className="flex items-center justify-center h-full w-full bg-amarillo/20 rounded-lg">
-                        <img src="/spain.jpg" alt="Spain" className="h-full w-full" />
-                        <h3 className="absolute text-2xl font-bold text-center mt-4">Spain</h3>
-                    </div>
+                {/* Tarjeta 3 */}
+                <div className="relative rounded-lg transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover" />
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
                 </div>
-                <div>
-                    <div className="flex items-center justify-center h-full w-full bg-amarillo/20 rounded-lg">
-                        <img src="/spain.jpg" alt="Spain" className="h-full w-full" />
-                        <h3 className="absolute text-2xl font-bold text-center mt-4">Spain</h3>
-                    </div>
+                {/* Tarjeta 4 */}
+                <div className="relative rounded-lg transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover"/>
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
                 </div>
-                <div>
-                    <div className="flex items-center justify-center h-full w-full bg-amarillo/20 rounded-lg">
-                        <img src="/spain.jpg" alt="Spain" className="h-full w-full" />
-                        <h3 className="absolute text-2xl font-bold text-center mt-4">Spain</h3>
-                    </div>
+                {/* Tarjeta 5 */}
+                <div className="relative rounded-lg transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover" />
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
+                </div>
+                {/* Tarjeta 6 */}
+                <div className="relative rounded-lg transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover" />
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
+                </div>
+                {/* Tarjeta 7 */}
+                <div className="relative rounded-lg transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover" />
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
+                </div>
+                {/* Tarjeta 8 */}
+                <div className="relative rounded-lg transform transition duration-300 hover:scale-105">
+                    <img src="/spain.jpg" alt="Spain" className=" rounded-lg w-full h-full object-cover"/>
+                    <h3
+                        className="absolute inset-0 flex items-center justify-center text-2xl font-bold text-gray-950 italic"
+                        style={{ textShadow: "2px 2px 3px rgba(65,105,225,0.7)" }}
+                    >
+                        Spain
+                    </h3>
                 </div>
             </div>
         </AnimatedOnScroll>
-    )
+    );
 }
