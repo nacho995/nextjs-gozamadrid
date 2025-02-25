cbdf355ad79b846cfaf717abef6c53af3da96bd9
nacho995 <ignaciodalesio1995@gmail.com>
2025-02-25 14:34:16 +0100

    arrelgoConBlogsYPropiedades

---
+++ frontend/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 'images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dv31mt6pd/image/upload/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+  // ... resto de tu configuración
+};
+
+module.exports = nextConfig; 
