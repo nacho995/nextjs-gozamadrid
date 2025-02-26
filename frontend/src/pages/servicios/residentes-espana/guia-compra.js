60e956afa6d593b58713910c5e58bd176916738e
nacho995 <ignaciodalesio1995@gmail.com>
2025-02-26 08:34:02 +0100

    cambioYarregloDeEstilos

--- frontend/src/pages/servicios/residentes-espana/guia-compra.js
+++ frontend/src/pages/servicios/residentes-espana/guia-compra.js
@@ -2,15 +2,24 @@ import GuiaCompraInmuebles from "@/components/Servicios/guiaCompraInmuebles";
 
 export default function GuiaCompra() {
     return (
-        <>
+        <div className="relative min-h-screen">
+            {/* Fondo fijo */}
             <div
-                className="fixed inset-0 z-0 opacity-100 bg-cover bg-center"
+                className="fixed inset-0 z-0"
                 style={{
                     backgroundImage: "url('/gozamadridwp.jpg')",
                     backgroundAttachment: "fixed",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
                 }}
-            ></div>
-            <GuiaCompraInmuebles />
-        </>
+            />
+            
+            {/* Contenedor principal con z-index superior */}
+            <div className="relative z-10">
+                <div className="container mx-auto px-4 py-16">
+                    <GuiaCompraInmuebles />
+                </div>
+            </div>
+        </div>
     );
 }
