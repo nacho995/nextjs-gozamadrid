1085e09004cf8a65711c2f12cc597fa32391a52a
nacho995 <ignaciodalesio1995@gmail.com>
2025-02-26 15:45:49 +0100

    Fix vulnerabilities

--- frontend/src/pages/servicios/residentes-espana/index.js
+++ frontend/src/pages/servicios/residentes-espana/index.js
@@ -5,7 +5,7 @@ export default function ResidentesEspaña() {
     return (
         <>
             <div
-                className="fixed inset-0 z-0 opacity-100 bg-cover bg-center"
+                className="fixed inset-0 z-0 opacity-100"
                 style={{
                     backgroundImage: "url('/gozamadridwp2.jpg')",
                     backgroundAttachment: "fixed",
