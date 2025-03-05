43c1329999124305067e02e6ddd6be4f2173684a
nacho995 <ignaciodalesio1995@gmail.com>
2025-03-05 15:05:41 +0100

    Produccion

--- frontend/src/pages/api.js
+++ frontend/src/pages/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_LOCAL_API_URL || 'http://localhost:4000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
 
 export async function getCountryPrefix() {
   try {
