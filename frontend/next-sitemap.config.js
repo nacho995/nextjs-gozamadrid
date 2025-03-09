f5b802e609c6e01d3c848f11601a282295e2a99e
nacho995 <ignaciodalesio1995@gmail.com>
2025-03-09 18:03:20 +0100

    Produccion

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://gozamadrid.com',
+  generateRobotsTxt: true,
+  exclude: ['/admin/*', '/api/*'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}; 
