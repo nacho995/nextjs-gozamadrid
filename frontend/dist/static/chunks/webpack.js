32f27b6442ec794088ff0d543d58f3bfbaab9b6a
nacho995 <ignaciodalesio1995@gmail.com>
2025-03-11 13:52:44 +0100

    Produccion

--- frontend/dist/static/chunks/webpack.js
+++ frontend/dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c8582584616134a"; }
+/******/ 		__webpack_require__.h = function() { return "e311171e4f13cec7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/components/LoadingScreen.jsx
+++ frontend/src/components/LoadingScreen.jsx
@@ -17,10 +17,22 @@ const LoadingScreen = ({
         className="bg-white/10 backdrop-blur-sm p-8 rounded-xl shadow-2xl flex flex-col items-center"
         role="status"
       >
-        <div 
-          className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-amber-400 mb-4"
-          aria-hidden="true"
-        ></div>
+        <div className="relative h-16 w-32 mb-4">
+          {/* Círculo izquierdo */}
+          <div 
+            className="absolute left-0 top-0 w-16 h-16 border-4 border-amarillo rounded-full"
+            style={{
+              animation: 'infinityLeft 2s linear infinite',
+            }}
+          ></div>
+          {/* Círculo derecho */}
+          <div 
+            className="absolute right-0 top-0 w-16 h-16 border-4 border-gray-400 rounded-full"
+            style={{
+              animation: 'infinityRight 2s linear infinite',
+            }}
+          ></div>
+        </div>
         <p 
           className="text-amber-400 text-xl font-semibold" 
           style={{ textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)' }}
