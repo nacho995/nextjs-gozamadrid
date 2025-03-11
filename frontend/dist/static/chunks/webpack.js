d13d444f9e0809ca9fc3066a801460ac7d74bb98
nacho995 <ignaciodalesio1995@gmail.com>
2025-03-11 15:05:07 +0100

    Produccion

--- frontend/dist/static/chunks/webpack.js
+++ frontend/dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e311171e4f13cec7"; }
+/******/ 		__webpack_require__.h = function() { return "02b85d74b751b04d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
