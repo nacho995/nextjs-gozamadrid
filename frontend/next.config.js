c7b3ae8c7cee5d9d4074aa56f13180c444f1da26
nacho995 <ignaciodalesio1995@gmail.com>
2025-03-20 01:04:22 +0100

    Fix: Solucionar problema de 'Ignored Build Step' en Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,12 +9,6 @@ const nextConfig = {
   basePath: '',
   distDir: 'dist',
   // La opción experimental no es necesaria ya que estamos usando solo Pages Router
-  exportPathMap: async function() {
-    return {
-      '/': { page: '/' },
-      '/404': { page: '/404' }
-    };
-  }
 }
 
 module.exports = nextConfig;
